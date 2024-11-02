c6e4949df46fd5db4eb620eaa43ba9cf29dcb1bf
Borja Muñoz <bmunoz@cartodb.com>
2024-11-02 01:13:55 +0100

    Add literals and translations for player summary panel

--- web-app/src/i18n/messages.ts
+++ web-app/src/i18n/messages.ts
@@ -14,6 +14,24 @@ export const messages = {
 
     // Dashboard
     players: "Players",
+
+    // Player Summary
+    playerSummary: "Player Summary",
+    matchesPlayed: "Matches Played",
+    matchesWon: "Matches Won",
+    matchesLost: "Matches Lost",
+    setsWon: "Sets Won",
+    setsLost: "Sets Lost",
+    setsBalance: "Set Balance",
+    gamesWon: "Games Won",
+    gamesLost: "Games Lost",
+    gamesBalance: "Game Balance",
+    superTieBreaksWon: "Super Tie Breaks Won",
+    superTieBreaksLost: "Super Tie Breaks Lost",
+    matchesRetired: "Matches Retired",
+
+    // Cycles
+    cicles: "Cycles",
   },
 
   [LOCALES.SPANISH]: {
@@ -28,6 +46,24 @@ export const messages = {
 
     // Dashboard
     players: "Jugadores",
+
+    // Player Summary
+    playerSummary: "Resumen",
+    matchesPlayed: "Partidos Jugados",
+    matchesWon: "Partidos Ganados",
+    matchesLost: "Partidos Perdidos",
+    setsWon: "Sets Ganados",
+    setsLost: "Sets Perdidos",
+    setsBalance: "Balance Sets",
+    gamesWon: "Juegos Ganados",
+    gamesLost: "Juegos Perdidos",
+    gamesBalance: "Balance Juegos",
+    superTieBreaksWon: "Super Tie Breaks Ganados",
+    superTieBreaksLost: "Super Tie Breaks Perdidos",
+    matchesRetired: "Partidos Retirados",
+
+    // Cycles
+    cycles: "Ciclos",
   },
 
 };
