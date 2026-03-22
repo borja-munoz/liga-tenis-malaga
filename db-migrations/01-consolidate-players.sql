1cf6e3fa55305bd3dea69b2b57baf8f010ed6b38
Borja Muñoz <borja.munoz@gmail.com>
2026-03-22 16:23:54 +0100

    Add script to consolidate players across seasons (same person with different name)

---
+++ db-migrations/01-consolidate-players.sql
@@ -0,0 +1,517 @@
+-------------------------------
+-- Adrián Gómez
+-------------------------------
+
+UPDATE seasons_players
+SET player_id = 5
+WHERE player_id IN (4, 6, 388, 392);
+
+UPDATE cycle_standings
+SET player_id = 5
+WHERE player_id IN (4, 6, 388, 392);
+
+UPDATE results_cycles
+SET player_a_id = 5
+WHERE player_a_id IN (4, 6, 388, 392);
+
+UPDATE results_cycles
+SET player_b_id = 5
+WHERE player_b_id IN (4, 6, 388, 392);
+
+UPDATE results_cycles
+SET super_tie_break_winner_id = 5
+WHERE super_tie_break_winner_id IN (4, 6, 388, 392);
+
+UPDATE results_cycles
+SET retired_player_id = 5
+WHERE retired_player_id IN (4, 6, 388, 392);
+
+UPDATE results_playoffs
+SET player_a_id = 5
+WHERE player_a_id IN (4, 6, 388, 392);
+
+UPDATE results_playoffs
+SET player_b_id = 5
+WHERE player_b_id IN (4, 6, 388, 392);
+
+UPDATE results_playoffs
+SET super_tie_break_winner_id = 5
+WHERE super_tie_break_winner_id IN (4, 6, 388, 392);
+
+UPDATE results_playoffs
+SET retired_player_id = 5
+WHERE retired_player_id IN (4, 6, 388, 392);
+
+DELETE FROM players
+WHERE id IN (4, 6, 388, 392);
+
+-------------------------------
+-- Adrián Mariscal
+-------------------------------
+
+UPDATE seasons_players
+SET player_id = 8
+WHERE player_id IN (389);
+
+UPDATE cycle_standings
+SET player_id = 8
+WHERE player_id IN (389);
+
+UPDATE results_cycles
+SET player_a_id = 8
+WHERE player_a_id IN (389);
+
+UPDATE results_cycles
+SET player_b_id = 8
+WHERE player_b_id IN (389);
+
+UPDATE results_cycles
+SET super_tie_break_winner_id = 8
+WHERE super_tie_break_winner_id IN (389);
+
+UPDATE results_cycles
+SET retired_player_id = 8
+WHERE retired_player_id IN (389);
+
+UPDATE results_playoffs
+SET player_a_id = 8
+WHERE player_a_id IN (389);
+
+UPDATE results_playoffs
+SET player_b_id = 8
+WHERE player_b_id IN (389);
+
+UPDATE results_playoffs
+SET super_tie_break_winner_id = 8
+WHERE super_tie_break_winner_id IN (389);
+
+UPDATE results_playoffs
+SET retired_player_id = 8
+WHERE retired_player_id IN (389);
+
+DELETE FROM players
+WHERE id IN (389);
+
+-------------------------------
+-- Adriano Petrizzo
+-------------------------------
+
+UPDATE seasons_players
+SET player_id = 3
+WHERE player_id IN (390, 391);
+
+UPDATE cycle_standings
+SET player_id = 3
+WHERE player_id IN (390, 391);
+
+UPDATE results_cycles
+SET player_a_id = 3
+WHERE player_a_id IN (390, 391);
+
+UPDATE results_cycles
+SET player_b_id = 3
+WHERE player_b_id IN (390, 391);
+
+UPDATE results_cycles
+SET super_tie_break_winner_id = 3
+WHERE super_tie_break_winner_id IN (390, 391);
+
+UPDATE results_cycles
+SET retired_player_id = 3
+WHERE retired_player_id IN (390, 391);
+
+UPDATE results_playoffs
+SET player_a_id = 3
+WHERE player_a_id IN (390, 391);
+
+UPDATE results_playoffs
+SET player_b_id = 3
+WHERE player_b_id IN (390, 391);
+
+UPDATE results_playoffs
+SET super_tie_break_winner_id = 3
+WHERE super_tie_break_winner_id IN (390, 391);
+
+UPDATE results_playoffs
+SET retired_player_id = 3
+WHERE retired_player_id IN (390, 391);
+
+DELETE FROM players
+WHERE id IN (390, 391);
+
+-------------------------------
+-- Alberto Álvarez
+-------------------------------
+
+UPDATE seasons_players
+SET player_id = 19
+WHERE player_id IN (20, 393);
+
+UPDATE cycle_standings
+SET player_id = 19
+WHERE player_id IN (20, 393);
+
+UPDATE results_cycles
+SET player_a_id = 19
+WHERE player_a_id IN (20, 393);
+
+UPDATE results_cycles
+SET player_b_id = 19
+WHERE player_b_id IN (20, 393);
+
+UPDATE results_cycles
+SET super_tie_break_winner_id = 19
+WHERE super_tie_break_winner_id IN (20, 393);
+
+UPDATE results_cycles
+SET retired_player_id = 19
+WHERE retired_player_id IN (20, 393);
+
+UPDATE results_playoffs
+SET player_a_id = 19
+WHERE player_a_id IN (20, 393);
+
+UPDATE results_playoffs
+SET player_b_id = 19
+WHERE player_b_id IN (20, 393);
+
+UPDATE results_playoffs
+SET super_tie_break_winner_id = 19
+WHERE super_tie_break_winner_id IN (20, 393);
+
+UPDATE results_playoffs
+SET retired_player_id = 19
+WHERE retired_player_id IN (20, 393);
+
+DELETE FROM players
+WHERE id IN (20, 393);
+
+-------------------------------
+-- Alberto Vázquez
+-------------------------------
+
+UPDATE seasons_players
+SET player_id = 18
+WHERE player_id IN (394);
+
+UPDATE cycle_standings
+SET player_id = 18
+WHERE player_id IN (394);
+
+UPDATE results_cycles
+SET player_a_id = 18
+WHERE player_a_id IN (394);
+
+UPDATE results_cycles
+SET player_b_id = 18
+WHERE player_b_id IN (394);
+
+UPDATE results_cycles
+SET super_tie_break_winner_id = 18
+WHERE super_tie_break_winner_id IN (394);
+
+UPDATE results_cycles
+SET retired_player_id = 18
+WHERE retired_player_id IN (394);
+
+UPDATE results_playoffs
+SET player_a_id = 18
+WHERE player_a_id IN (394);
+
+UPDATE results_playoffs
+SET player_b_id = 18
+WHERE player_b_id IN (394);
+
+UPDATE results_playoffs
+SET super_tie_break_winner_id = 18
+WHERE super_tie_break_winner_id IN (394);
+
+UPDATE results_playoffs
+SET retired_player_id = 18
+WHERE retired_player_id IN (394);
+
+DELETE FROM players
+WHERE id IN (394);
+
+-------------------------------
+-- Alejandro Gallardo
+-------------------------------
+
+UPDATE seasons_players
+SET player_id = 22
+WHERE player_id IN (23);
+
+UPDATE cycle_standings
+SET player_id = 22
+WHERE player_id IN (23);
+
+UPDATE results_cycles
+SET player_a_id = 22
+WHERE player_a_id IN (23);
+
+UPDATE results_cycles
+SET player_b_id = 22
+WHERE player_b_id IN (23);
+
+UPDATE results_cycles
+SET super_tie_break_winner_id = 22
+WHERE super_tie_break_winner_id IN (23);
+
+UPDATE results_cycles
+SET retired_player_id = 22
+WHERE retired_player_id IN (23);
+
+UPDATE results_playoffs
+SET player_a_id = 22
+WHERE player_a_id IN (23);
+
+UPDATE results_playoffs
+SET player_b_id = 22
+WHERE player_b_id IN (23);
+
+UPDATE results_playoffs
+SET super_tie_break_winner_id = 22
+WHERE super_tie_break_winner_id IN (23);
+
+UPDATE results_playoffs
+SET retired_player_id = 22
+WHERE retired_player_id IN (23);
+
+DELETE FROM players
+WHERE id IN (23);
+
+-------------------------------
+-- Alejandro García
+-------------------------------
+
+UPDATE seasons_players
+SET player_id = 25
+WHERE player_id IN (24);
+
+UPDATE cycle_standings
+SET player_id = 25
+WHERE player_id IN (24);
+
+UPDATE results_cycles
+SET player_a_id = 25
+WHERE player_a_id IN (24);
+
+UPDATE results_cycles
+SET player_b_id = 25
+WHERE player_b_id IN (24);
+
+UPDATE results_cycles
+SET super_tie_break_winner_id = 25
+WHERE super_tie_break_winner_id IN (24);
+
+UPDATE results_cycles
+SET retired_player_id = 25
+WHERE retired_player_id IN (24);
+
+UPDATE results_playoffs
+SET player_a_id = 25
+WHERE player_a_id IN (24);
+
+UPDATE results_playoffs
+SET player_b_id = 25
+WHERE player_b_id IN (24);
+
+UPDATE results_playoffs
+SET super_tie_break_winner_id = 25
+WHERE super_tie_break_winner_id IN (24);
+
+UPDATE results_playoffs
+SET retired_player_id = 25
+WHERE retired_player_id IN (24);
+
+DELETE FROM players
+WHERE id IN (24);
+
+-------------------------------
+-- Alejandro Gómez
+-------------------------------
+
+UPDATE seasons_players
+SET player_id = 26
+WHERE player_id IN (395);
+
+UPDATE cycle_standings
+SET player_id = 26
+WHERE player_id IN (395);
+
+UPDATE results_cycles
+SET player_a_id = 26
+WHERE player_a_id IN (395);
+
+UPDATE results_cycles
+SET player_b_id = 26
+WHERE player_b_id IN (395);
+
+UPDATE results_cycles
+SET super_tie_break_winner_id = 26
+WHERE super_tie_break_winner_id IN (395);
+
+UPDATE results_cycles
+SET retired_player_id = 26
+WHERE retired_player_id IN (395);
+
+UPDATE results_playoffs
+SET player_a_id = 26
+WHERE player_a_id IN (395);
+
+UPDATE results_playoffs
+SET player_b_id = 26
+WHERE player_b_id IN (395);
+
+UPDATE results_playoffs
+SET super_tie_break_winner_id = 26
+WHERE super_tie_break_winner_id IN (395);
+
+UPDATE results_playoffs
+SET retired_player_id = 26
+WHERE retired_player_id IN (395);
+
+DELETE FROM players
+WHERE id IN (395);
+
+-------------------------------
+-- Alexey Metalnikov
+-------------------------------
+
+UPDATE seasons_players
+SET player_id = 35
+WHERE player_id IN (396);
+
+UPDATE cycle_standings
+SET player_id = 35
+WHERE player_id IN (396);
+
+UPDATE results_cycles
+SET player_a_id = 35
+WHERE player_a_id IN (396);
+
+UPDATE results_cycles
+SET player_b_id = 35
+WHERE player_b_id IN (396);
+
+UPDATE results_cycles
+SET super_tie_break_winner_id = 35
+WHERE super_tie_break_winner_id IN (396);
+
+UPDATE results_cycles
+SET retired_player_id = 35
+WHERE retired_player_id IN (396);
+
+UPDATE results_playoffs
+SET player_a_id = 35
+WHERE player_a_id IN (396);
+
+UPDATE results_playoffs
+SET player_b_id = 35
+WHERE player_b_id IN (396);
+
+UPDATE results_playoffs
+SET super_tie_break_winner_id = 35
+WHERE super_tie_break_winner_id IN (396);
+
+UPDATE results_playoffs
+SET retired_player_id = 35
+WHERE retired_player_id IN (396);
+
+DELETE FROM players
+WHERE id IN (396);
+
+-------------------------------
+-- Alessandro Giannini
+-------------------------------
+
+UPDATE seasons_players
+SET player_id = 32
+WHERE player_id IN (31);
+
+UPDATE cycle_standings
+SET player_id = 32
+WHERE player_id IN (31);
+
+UPDATE results_cycles
+SET player_a_id = 32
+WHERE player_a_id IN (31);
+
+UPDATE results_cycles
+SET player_b_id = 32
+WHERE player_b_id IN (31);
+
+UPDATE results_cycles
+SET super_tie_break_winner_id = 32
+WHERE super_tie_break_winner_id IN (31);
+
+UPDATE results_cycles
+SET retired_player_id = 32
+WHERE retired_player_id IN (31);
+
+UPDATE results_playoffs
+SET player_a_id = 32
+WHERE player_a_id IN (31);
+
+UPDATE results_playoffs
+SET player_b_id = 32
+WHERE player_b_id IN (31);
+
+UPDATE results_playoffs
+SET super_tie_break_winner_id = 32
+WHERE super_tie_break_winner_id IN (31);
+
+UPDATE results_playoffs
+SET retired_player_id = 32
+WHERE retired_player_id IN (31);
+
+DELETE FROM players
+WHERE id IN (31);
+
+-------------------------------
+-- Andrés García
+-------------------------------
+
+UPDATE seasons_players
+SET player_id = 49
+WHERE player_id IN (48, 398);
+
+UPDATE cycle_standings
+SET player_id = 49
+WHERE player_id IN (48, 398);
+
+UPDATE results_cycles
+SET player_a_id = 49
+WHERE player_a_id IN (48, 398);
+
+UPDATE results_cycles
+SET player_b_id = 49
+WHERE player_b_id IN (48, 398);
+
+UPDATE results_cycles
+SET super_tie_break_winner_id = 49
+WHERE super_tie_break_winner_id IN (48, 398);
+
+UPDATE results_cycles
+SET retired_player_id = 49
+WHERE retired_player_id IN (48, 398);
+
+UPDATE results_playoffs
+SET player_a_id = 49
+WHERE player_a_id IN (48, 398);
+
+UPDATE results_playoffs
+SET player_b_id = 49
+WHERE player_b_id IN (48, 398);
+
+UPDATE results_playoffs
+SET super_tie_break_winner_id = 49
+WHERE super_tie_break_winner_id IN (48, 398);
+
+UPDATE results_playoffs
+SET retired_player_id = 49
+WHERE retired_player_id IN (48, 398);
+
+DELETE FROM players
+WHERE id IN (48, 398);
+
